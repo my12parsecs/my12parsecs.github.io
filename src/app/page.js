766a37f54ef5020966f97ec80ad0e8eec03d8285
Hikaru <105549697+my12parsecs@users.noreply.github.com>
2024-11-23 13:33:06 +0900

    css

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,8 @@
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] mt-0">
-      <main className="flex flex-col row-start-2 items-center justify-center">
+    <div className="flex items-center justify-items-center min-h-screen m-auto">
+      <main className="flex flex-col row-start-2 items-center justify-center m-auto pb-10">
         <h1 className="text-3xl font-mono">Hikaru Imai</h1>
         <div className="mt-2 font-mono"><span>What I am working on: </span><a href="https://bokenmap.com" target="_blank" className="underline text-green-500">Bokenmap</a></div>
         <img src="https://drscdn.500px.org/photo/1081066081/q%3D80_m%3D1500/v2?sig=a252e8aef70ce8d2611ef23590db4e6122f566505ba6fe27bac6587411b4c7e3" alt="" className="mt-5 w-4/5 max-w-5xl" />
