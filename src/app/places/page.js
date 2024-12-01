6ff1fb1fc3a04306857622dae65e33d17e378a3f
Hikaru <105549697+my12parsecs@users.noreply.github.com>
2024-12-01 18:26:09 +0900

    places

---
+++ src/app/places/page.js
@@ -0,0 +1,11 @@
+export default function Places() {
+    return (
+      <div className="w-3/4 m-auto mt-20">
+        <h2 className="text-lg font-bold">Want to go</h2>
+        <ul>
+            <li>Luang Prabang, Laos</li>
+        </ul>
+      </div>
+    );
+  }
+  
